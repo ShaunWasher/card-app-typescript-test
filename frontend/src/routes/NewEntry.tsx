dacf9c3018a05c5d99f7bb3767401ba5b83f8506
Shaun Washer <119515237+ShaunWasher@users.noreply.github.com>
2024-10-27 16:54:25 +0000

    feat: Added dark mode

--- frontend/src/routes/NewEntry.tsx
+++ frontend/src/routes/NewEntry.tsx
@@ -17,11 +17,11 @@ export default function NewEntry(){
         setNewEntry(emptyEntry)
     }
     return(
-        <section className="flex justify-center flex-col w-fit ml-auto mr-auto mt-10 gap-5 bg-gray-300 p-8 rounded-md">
-            <input className="p-3 rounded-md" type="text" placeholder="Title" name="title" value={newEntry.title} onChange={handleInputChange}/>
-            <textarea className="p-3 rounded-md" placeholder="Description" name="description" value={newEntry.description} onChange={handleInputChange}/>
-            <input className="p-3 rounded-md" type="date" name="created_at" value={(new Date(newEntry.created_at)).toISOString().split('T')[0]} onChange={handleInputChange}/>
-            <button onClick={(e) => {handleSend(e)}} className="bg-blue-400 hover:bg-blue-600 font-semibold text-white p-3 rounded-md">Create</button>
+        <section className="flex justify-center flex-col w-fit ml-auto mr-auto mt-10 gap-5 bg-gray-300 dark:bg-gray-800 p-8 rounded-md">
+            <input className="p-3 rounded-md dark:bg-black" type="text" placeholder="Title" name="title" value={newEntry.title} onChange={handleInputChange}/>
+            <textarea className="p-3 rounded-md dark:bg-black" placeholder="Description" name="description" value={newEntry.description} onChange={handleInputChange}/>
+            <input className="p-3 rounded-md dark:bg-black" type="date" name="created_at" value={(new Date(newEntry.created_at)).toISOString().split('T')[0]} onChange={handleInputChange}/>
+            <button onClick={(e) => {handleSend(e)}} className="bg-blue-400 hover:bg-blue-600 font-semibold text-white dark:text-black p-3 rounded-md">Create</button>
         </section>
     )
 }
---
+++ frontend/src/routes/Settings.tsx
@@ -0,0 +1,5 @@
+export default function Settings(){
+    return(
+        <button onClick={()=> {document.documentElement.classList.toggle("dark")}} className="self-center m-1 md:m-2 p-1  rounded-md bg-slate-800 hover:bg-slate-600 dark:bg-slate-200 dark:hover:bg-slate-400 font-medium text-white dark:text-black">toggle dark mode</button>
+    )
+}
