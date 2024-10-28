16fc22f0ecea808c1aefcd8e8d76cc2640c3ac18
Shaun Washer <119515237+ShaunWasher@users.noreply.github.com>
2024-10-28 18:30:46 +0000

    Added tests to the backend

--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -5,4 +5,34 @@ describe("server test", () => {
   it("should assert 1 + 1 is 2", () => {
     expect(1 + 1).toEqual(2);
   });
+
+  it("should respond to a GET request", async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/get/'
+    })
+    expect(response.statusCode).toEqual(200);
+  });
+
+  it("should create and delete an element", async () => {
+    const responseCreate = await server.inject({
+      method: 'POST',
+      url: '/create/',
+      payload: {title: "test card", description: "description", created_at: "2024-10-28", scheduled_date: "2024-10-28"}
+    })
+    expect(responseCreate.statusCode).toEqual(200);
+    const card = await Prisma.entry.findFirst({
+      where: {title: "test card", description: "description", created_at: new Date("2024-10-28"), scheduled_date: new Date("2024-10-28")},
+    });
+    expect(card).not.toBeNull();
+    const responseDelete = await server.inject({
+      method: 'DELETE',
+      url: '/delete/'+card?.id.toString()
+    })
+    expect(responseDelete.statusCode).toEqual(200);
+    const deletedCard = await Prisma.entry.findFirst({
+      where: {title: "test card", description: "description", created_at: new Date("2024-10-28"), scheduled_date: new Date("2024-10-28")},
+    });
+    expect(deletedCard).toBeNull();
+  });
 });
